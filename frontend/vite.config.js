90de4c68bd4f4542d32ac826e1c956eff42a25cd
maheshgajjela221 <166375267+maheshgajjela221@users.noreply.github.com>
2025-10-18 14:20:12 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,9 +4,10 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5173,
+    host: "0.0.0.0",      // 👈 Allows external access
+    port: 5175,
     proxy: {
-      "/api": "http://localhost:3000"
-    }
-  }
+      "/api": "http://52.64.85.12:3175", // backend API
+    },
+  },
 });
